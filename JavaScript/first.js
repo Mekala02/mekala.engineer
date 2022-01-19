fb9619c55f4510cc1c21e74026dccb6e593d32cd
Mehmet Eren Kala <43720526+Mekala02@users.noreply.github.com>
2022-01-19 08:21:42 +0300

    Initial Commit
    Initial Commit

---
+++ JavaScript/first.js
@@ -0,0 +1,28 @@
+var item = document.querySelectorAll(".skilmark");
+item[0].addEventListener("mouseover", func, false);
+item[0].addEventListener("mouseout", func1, false);
+
+var asd = "50"
+let test = document.querySelector('#test');
+let witdh = "10"
+function func()
+{
+  var div = document.createElement("div");
+  div.id = "temp";
+  div.style.position = "absolute";
+  div.style.margin = "0";
+  div.style.width = witdh + "%";
+  div.style.height = "100%";
+  div.style.backgroundColor = "#4900c9bb";
+  div.style.borderRadius = "30px 0 0 0";
+  div.style.color = "white";
+  div.innerHTML = "%" + witdh;
+  div.style.textAlign = "center";
+  document.getElementById("about").appendChild(div);
+}
+
+function func1()
+{
+   var temp = document.getElementById("temp");
+   temp.remove();
+}
